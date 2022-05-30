bf254b9314a7b9adc4044b2682bfc5f3aa0b13a4
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2022-05-30 17:25:59 -0600

    md file renamed

--- Develop/index.js
+++ Develop/index.js
@@ -75,7 +75,7 @@ const questions = [
       {
         type: 'list',
         message: 'Please select which license did you use for this project:',
-        choices: ['GNU AGPLv3','GNU GPLv3','GNU LGPLv3','Mozilla Public License 2.0','Apache License 2.0','MIT License','Boost Software License 1.0','The Unlicense'],
+        choices: ['GNU AGPLv3','GNU GPLv3','GNU LGPLv3','Mozilla Public License 2.0','Apache License 2.0','MIT License','Boost Software License 1.0','The Unlicense', ''],
         name: 'license',
         validate: (answer) => {
             if(answer){
